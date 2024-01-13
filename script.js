a677202a9c05da8ed9847c0a1814c6143a9c6599
svivek19 <svivek030503@gmail.com>
2024-01-13 18:18:51 +0530

    feat: get day image based on date

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const dateFormEl = document.querySelector("#dateForm");
 const dayContainerEl = document.querySelector("#dayImg");
 
-const myDate = new Date();
 
-const res = myDate.toLocaleDateString("en-IN", {
-    year: "numeric",
-    month: "2-digit",
-    day: "numeric",
-});
+dateFormEl.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-let formetDate = res.replaceAll("/", "");
-const formetUrl = `https://www.tamildailycalendar.com/${myDate.getFullYear()}/${formetDate}.jpg`;
+    const inputDateEl = e.target[0].value;
+    // console.log(inputEl);
+
+    processDate(inputDateEl)
+})
+
+function processDate(date) {
+
+    const myDate = date ? new Date(date) : new Date();
+    
+    const res = myDate.toLocaleDateString("en-IN", {
+        year: "numeric",
+        month: "2-digit",
+        day: "numeric",
+    });
+
+    let formetDate = res.replaceAll("/", "");
+    const formetUrl = `https://www.tamildailycalendar.com/${myDate.getFullYear()}/${formetDate}.jpg`;
 
 
-dayContainerEl.src = formetUrl;
-dayContainerEl.alt = formetDate;
+    dayContainerEl.src = formetUrl;
+    dayContainerEl.alt = formetDate;
 
-console.log(formetUrl);
+}
 
+processDate()
